1d14f5c298dd8383825114b0e324b65775709c60
surajsabale8191 <surajsabale8191@gmail.com>
2026-08-09 19:40:52 +0530

    added templates directory and created home page made some changes in views.py

--- notes/views.py
+++ notes/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def notes_home(request):
-    return HttpResponse("Welcome to notes Talking App")
+    
+    context={
+        "name":"Suraj",
+        "course":"Django and DRF",
+        "Goal" : "Understand the Django ecosystem"
+    }
+    return render(request, "notes/home.html" ,context)
