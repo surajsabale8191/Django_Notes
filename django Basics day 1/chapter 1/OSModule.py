7d19ee729a4a5004f03af7be2898ecfab50bc939
surajsabale8191 <surajsabale8191@gmail.com>
2026-08-10 16:26:25 +0530

    Read Evalute print loop implementation program(REPL)

--- django Basics day 1/chapter 1/OSModule.py
+++ django Basics day 1/chapter 1/OSModule.py
@@ -1,6 +1,8 @@
 
 import os
-
+'''
+This is comment where we can add comment to the perticule task that we are going to done or for reference purpose
+'''
 def print_directory_contents(path="."):
     contents = os.listdir(path)
     for item in contents:
