e8d79fbd7c4b4f4f31e550b396855b821481ce9c
surajsabale8191 <surajsabale8191@gmail.com>
2026-08-10 17:00:07 +0530

    basics of variable and Datatypes

---
+++ django Basics day 1/chapter 2 Variables and Datatypes/typecasting.py
@@ -0,0 +1,24 @@
+#type() function and Typecasting
+     
+# type functions is used to find the data type of a given variable in python
+
+a=30
+print(type(a))
+
+b="40"
+print(type(b))
+
+#A number can be converted into a string and vice versa(if possible)
+# There are many functions to  convert one data type into another
+
+str(30)
+int("32")
+float(32)
+
+
+# Input Function
+# This function allows the user to take input form  the Keyboard as a String
+
+a=input("Enter Name:")
+
+
---
+++ django Basics day 1/chapter 2 Variables and Datatypes/variable.py
@@ -0,0 +1,34 @@
+# A variable is the name given to a memory location in a program. 
+# for example
+
+a=30
+b="Suraj"
+c=71.22
+
+# Data Types
+
+# Primarily there are following data types in Python
+# Floating point numbers
+# Integers
+# Strings
+# Booleans
+# None
+# Python is a fantastic language that automatically identifies the type of data for us.
+
+
+a = 71            #Identifies a as classs <int>
+b= 88.44          #Identifies b as class <float>
+name = "Suraj"    #Identifies name as class <str>
+
+
+# Operators in Python 
+
+# Following are Some common operators in Python 
+# Arithmetic operators=>  +,-,*,/ etc .
+# Assignment operators=>  =, +=, -= etc.
+# Comparison operators =>  ==, >,>=,<, <=, != etc.
+# Logical operators =>  and, or, not
+
+
+
+
