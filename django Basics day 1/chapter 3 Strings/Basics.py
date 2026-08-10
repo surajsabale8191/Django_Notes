5014057121bba78f722900460e0b7a6bbdbd801a
surajsabale8191 <surajsabale8191@gmail.com>
2026-08-10 18:32:42 +0530

    Basics strings and slicing program

---
+++ django Basics day 1/chapter 3 Strings/Basics.py
@@ -0,0 +1,38 @@
+# String is a data type in Python
+# URBAN EDCE
+# String is a sequience of charactes enclosed in quotes
+# We can primarily, write a string in these these ways
+   
+a = 'Sanju'  # single quoted strings
+b = "Doremon"  # Double quoted strings 
+c = '''Nobita'''  #Triple quoted Strings 
+
+
+
+#String Slicing:
+
+# A string in python can be sliced for getting a part of the string.
+# consider the following:
+#     name= HARRY => length=5
+
+#                     S  U  R  A  J
+# forward index       0  1  2  3  4
+# negative index     -5 -4 -3 -2 -1
+
+# The index in a string starts from 0 to (length -1) in python. In order to slice a string, we use the following syntax:
+    
+#     sl=name[ind_start : ind_end]
+    
+#     sl[0:3]  returns "SUR"
+#     sl[1:3]  retuens "UR"
+    
+# Negative Indices :
+#     Negative indies can also be used as shown in the above .-1 corresponds to the (length -1) index, -2 to (length -2)
+
+
+len () funcion 
+This function returns the length of the stung
+27
+len("Suraj")  returns 5
+    
+    
---
+++ django Basics day 1/chapter 3 Strings/slicing.py
@@ -0,0 +1,9 @@
+# a="Suraj"
+# print(len(a))
+
+a="I love my india"
+b=""
+for i in a:
+    b=i+b
+    
+print(b)
